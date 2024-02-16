ed1ac26c9ca49648e68a1767b28720b1688f137f
bissaliev <bissaliev21@gmail.com>
2024-02-16 09:56:57 +0400

    pep8

--- homework.py
+++ homework.py
@@ -13,36 +13,36 @@ from exceptions import EmptyListError, NonExistendStatusError, StatusError
 load_dotenv()
 
 
-PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
-TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
-TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+PRACTICUM_TOKEN = os.getenv("PRACTICUM_TOKEN")
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
 RETRY_TIME = 600
-ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
-HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
+ENDPOINT = "https://practicum.yandex.ru/api/user_api/homework_statuses/"
+HEADERS = {"Authorization": f"OAuth {PRACTICUM_TOKEN}"}
 
 
 HOMEWORK_STATUSES = {
-    'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
-    'reviewing': 'Работа взята на проверку ревьюером.',
-    'rejected': 'Работа проверена: у ревьюера есть замечания.'
+    "approved": "Работа проверена: ревьюеру всё понравилось. Ура!",
+    "reviewing": "Работа взята на проверку ревьюером.",
+    "rejected": "Работа проверена: у ревьюера есть замечания.",
 }
 
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 handler = logging.StreamHandler(sys.stdout)
-formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
 
 def send_message(bot, message):
     """Отправка сообщения в Телеграмм."""
-    message_error = 'Сообщение в телеграмм не отправлено'
+    message_error = "Сообщение в телеграмм не отправлено"
     try:
         bot.send_message(TELEGRAM_CHAT_ID, message)
-        logger.info('Сообщение отправлено.')
+        logger.info("Сообщение отправлено.")
     except TelegramError:
         logger.error(message_error)
 
@@ -51,14 +51,14 @@ def get_api_answer(current_timestamp):
     """Получение данных с API Яндекс Практикума."""
     try:
         timestamp = current_timestamp or int(time.time())
-        params = {'from_date': timestamp}
+        params = {"from_date": timestamp}
         response = requests.get(ENDPOINT, headers=HEADERS, params=params)
     except requests.exceptions.RequestException as error:
         logger.error(error)
         raise SystemExit(error)
     else:
         if response.status_code != HTTPStatus.OK:
-            message_error = 'Ответ API некорректен.'
+            message_error = "Ответ API некорректен."
             logger.error(message_error)
             raise StatusError(message_error)
         try:
@@ -72,39 +72,39 @@ def get_api_answer(current_timestamp):
 def check_response(response):
     """Проверка данных полученных с API."""
     if type(response) is not dict:
-        message_error = 'Response не является словарем.'
+        message_error = "Response не является словарем."
         logger.error(message_error)
         raise TypeError(message_error)
-    if type(response.get('homeworks')) is not list:
-        message_error = 'Homeworks не является списком.'
+    if type(response.get("homeworks")) is not list:
+        message_error = "Homeworks не является списком."
         logger.error(message_error)
         raise TypeError(message_error)
-    if response.get('homeworks') is None:
-        message_error = 'Ошибка ключа homeworks.'
+    if response.get("homeworks") is None:
+        message_error = "Ошибка ключа homeworks."
         logger.error(message_error)
         raise KeyError(message_error)
-    if len(response.get('homeworks')) == 0:
-        message_error = 'Список пуст.'
+    if len(response.get("homeworks")) == 0:
+        message_error = "Список пуст."
         logger.error(message_error)
         raise EmptyListError(message_error)
-    return response.get('homeworks')
+    return response.get("homeworks")
 
 
 def parse_status(homework):
     """Определение статуса Домашней работы."""
-    homework_name = homework.get('homework_name')
-    homework_status = homework.get('status')
+    homework_name = homework.get("homework_name")
+    homework_status = homework.get("status")
     if homework_status is None:
-        message_error = 'homework_status имеет пустое значение.'
+        message_error = "homework_status имеет пустое значение."
         logger.error(message_error)
         raise KeyError(message_error)
     if homework_name is None:
-        message_error = 'homework_name имеет пустое значение.'
+        message_error = "homework_name имеет пустое значение."
         logger.error(message_error)
         raise KeyError(message_error)
     if homework_status not in HOMEWORK_STATUSES:
         message_error = (
-            f'{homework_status} - недокументированный статус домашней работы.'
+            f"{homework_status} - недокументированный статус домашней работы."
         )
         logger.error(message_error)
         raise NonExistendStatusError(message_error)
@@ -114,16 +114,16 @@ def parse_status(homework):
 
 def check_tokens():
     """Проверка на наличие обязательных прерменных окружения."""
-    message_error = 'Отсутствие обязательных переменных окружения:'
+    message_error = "Отсутствие обязательных переменных окружения:"
     tokens = True
     if PRACTICUM_TOKEN is None:
-        logger.critical(f'{message_error} PRACTICUM_TOKEN')
+        logger.critical(f"{message_error} PRACTICUM_TOKEN")
         tokens = False
     if TELEGRAM_TOKEN is None:
-        logger.critical(f'{message_error} TELEGRAM_TOKEN')
+        logger.critical(f"{message_error} TELEGRAM_TOKEN")
         tokens = False
     if TELEGRAM_CHAT_ID is None:
-        logger.critical(f'{message_error} TELEGRAM_CHAT_ID')
+        logger.critical(f"{message_error} TELEGRAM_CHAT_ID")
         tokens = False
     return tokens
 
@@ -144,10 +144,10 @@ def main():
             if homeworks:
                 message = parse_status(homeworks[0])
                 time.sleep(RETRY_TIME)
-                logger.info('Сообщение отправлено.')
+                logger.info("Сообщение отправлено.")
 
         except Exception as error:
-            message = f'Сбой в работе программы: {error}'
+            message = f"Сбой в работе программы: {error}"
             if error_message is None or error.args != error_message.args:
                 send_message(bot, message)
                 error_message = error
@@ -157,5 +157,5 @@ def main():
             send_message(bot, message)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
