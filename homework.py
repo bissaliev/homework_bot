8caeb07e1dc807aa13d724e6b13c35fc5167bcf3
bissaliev <bissaliev21@gmail.com>
2024-02-16 10:51:53 +0400

    рефакторинг

--- homework.py
+++ homework.py
@@ -53,16 +53,20 @@ def get_api_answer(current_timestamp):
         timestamp = current_timestamp or int(time.time())
         params = {"from_date": timestamp}
         response = requests.get(ENDPOINT, headers=HEADERS, params=params)
+
     except requests.exceptions.RequestException as error:
         logger.error(error)
         raise SystemExit(error)
+
     else:
         try:
             if response.status_code != HTTPStatus.OK:
                 message_error = "Ответ API некорректен."
                 logger.error(message_error)
                 raise StatusError(message_error)
+
             return response.json()
+
         except requests.JSONDecodeError as error:
             logger.error(error)
             raise error
@@ -74,18 +78,22 @@ def check_response(response):
         message_error = "Response не является словарем."
         logger.error(message_error)
         raise TypeError(message_error)
+
     if type(response.get("homeworks")) is not list:
         message_error = "Homeworks не является списком."
         logger.error(message_error)
         raise TypeError(message_error)
+
     if response.get("homeworks") is None:
         message_error = "Ошибка ключа homeworks."
         logger.error(message_error)
         raise KeyError(message_error)
+
     if len(response.get("homeworks")) == 0:
         message_error = "Список пуст."
         logger.error(message_error)
         raise EmptyListError(message_error)
+
     return response.get("homeworks")
 
 
@@ -97,32 +105,38 @@ def parse_status(homework):
         message_error = "homework_status имеет пустое значение."
         logger.error(message_error)
         raise KeyError(message_error)
+
     if homework_name is None:
         message_error = "homework_name имеет пустое значение."
         logger.error(message_error)
         raise KeyError(message_error)
+
     if homework_status not in HOMEWORK_STATUSES:
         message_error = (
             f"{homework_status} - недокументированный статус домашней работы."
         )
         logger.error(message_error)
         raise NonExistendStatusError(message_error)
+
     verdict = HOMEWORK_STATUSES.get(homework_status)
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
 def check_tokens():
-    """Проверка на наличие обязательных прерменных окружения."""
+    """Проверка на наличие обязательных переменных окружения."""
     message_error = "Отсутствие обязательных переменных окружения:"
     if PRACTICUM_TOKEN is None:
         logger.critical(f"{message_error} PRACTICUM_TOKEN")
         return False
+
     if TELEGRAM_TOKEN is None:
         logger.critical(f"{message_error} TELEGRAM_TOKEN")
         return False
+
     if TELEGRAM_CHAT_ID is None:
         logger.critical(f"{message_error} TELEGRAM_CHAT_ID")
         return False
+
     return True
 
 
@@ -130,6 +144,7 @@ def main():
     """Основная логика работы бота."""
     if not check_tokens():
         exit()
+
     bot = Bot(token=TELEGRAM_TOKEN)
     current_timestamp = int(time.time())
     error_message = None
@@ -149,8 +164,10 @@ def main():
             if error_message is None or error.args != error_message.args:
                 send_message(bot, message)
                 error_message = error
+
             logger.error(message)
             time.sleep(RETRY_TIME)
+
         else:
             send_message(bot, message)
 
